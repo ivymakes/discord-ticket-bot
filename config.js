59225381f5e0e85ecbe70025ac5e891c17f9e9b3
Ivy <epcnessvevo@gmail.com>
2026-08-03 01:09:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   // Role(s) that can see/claim tickets and get pinged when one opens.
   // Right-click a role -> Copy Role ID. Add as many as you want.
   supportRoleIds: [
-    // '123456789012345678',
-    // '234567890123456789',
+    '123456789012345678',
+    '876543210987654321',
   ],
 
   // Channel category tickets get created under. Right-click the category -> Copy Category ID.
